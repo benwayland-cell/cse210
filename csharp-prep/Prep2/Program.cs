35aa5e2a6fd3a86ce245d500384c2889aade4d33
benwayland-cell <ben.wayland2007@gmail.com>
2025-10-29 14:20:33 -0600

    Work for W01 Prove

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Input grade: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letterSign = "";
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            letterSign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            letterSign = "-";
+        }
+        
+
+        string letter;
+        Boolean passed = true;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            if (letterSign == "+")
+            {
+                letterSign = "";
+            }
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            passed = false;
+        }
+        else
+        {
+            letter = "F";
+            passed = false;
+            letterSign = "";
+        }
+
+        Console.WriteLine($"Your grade letter: {letter}{letterSign}");
+
+        if (passed)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed :(");
+        }
     }
 }
