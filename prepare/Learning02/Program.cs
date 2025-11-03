f19951b2d7af8b3d48b25cfdb50450be7d5eb71a
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-03 09:59:46 -0700

    Work on Learning Activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "company_name";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2000;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "company2";
+        job2._jobTitle = "Job title";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+
+        List<Job> jobList = new List<Job>();
+        jobList.Add(job1);
+        jobList.Add(job2);
+
+        Resume resume1 = new Resume();
+        resume1._name = "name";
+        resume1._jobs = jobList;
+        resume1.Display();
     }
 }
