1935932691597547e1e8179b49c20e08bce34fac
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-05 11:10:26 -0700

    added code to add an entry to Journal

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,11 +1,30 @@
 
 class Journal
 {
-    public List<Journal> _entryList;
+    public List<JournalEntry> _entryList;
+
+    public void AddEntry()
+    {   
+        /* Gets input from the user and stores the data */
+        
+        // get the data to add to the entry
+        string date = "11/5/2025";
+        Console.Write("Prompt: ");
+        string prompt = Console.ReadLine();
+        Console.Write("Entry: ");
+        string entry = Console.ReadLine();
+
+        // set up the journal entry
+        JournalEntry entryToAdd = new JournalEntry();
+        entryToAdd.Set(date, prompt, entry);
+
+        // Add the entry to the list
+        _entryList.Add(entryToAdd);
+    }
 
     public void Display()
     {
-        foreach (Journal entry in _entryList)
+        foreach (JournalEntry entry in _entryList)
         {
             entry.Display();
             Console.WriteLine();
