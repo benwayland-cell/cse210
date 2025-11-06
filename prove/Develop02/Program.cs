835e9e9dbfc55f5a7a74e0a36b2675165088e0f5
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-06 15:32:35 -0700

    Changed a few comments and made it so the user inputs a filename

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,16 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Initialize the menu and the journal
         Menu journalMenu = new Menu();
         Journal currentJournal = new Journal();
 
-        // JournalEntry testJournalEntry = journalMenu.CreateJournalEntry();
-        // testJournalEntry.Display();
-
-
+        // the main loop
         bool done = false;
         do
         {
+            // Get what the user wants to do
             int userSelection = journalMenu.ProcessMenu();
 
             switch (userSelection)
@@ -29,11 +28,11 @@ class Program
                     break;
                 case 3:
                     // Save to a file
-                    currentJournal.SaveToFile("test");
+                    currentJournal.SaveToFile(journalMenu.GetFileNameFromUser());
                     break;
                 case 4:
                     // Read from a file
-                    currentJournal.ReadFromFile("test");
+                    currentJournal.ReadFromFile(journalMenu.GetFileNameFromUser());
                     break;
                 case 5:
                     // Quit
