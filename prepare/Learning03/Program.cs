1a3bf1592858e7045b25e28d6f533c04ae5ad1d6
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-11 09:51:28 -0700

    Finished testing main program.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,15 +6,30 @@ class Program
     {
         Fraction testFraction1 = new Fraction();
         Fraction testFraction2 = new Fraction(5);
-        Fraction testFraction3 = new Fraction(5, 10);
+        Fraction testFraction3 = new Fraction(2, 10);
 
+        Console.WriteLine(testFraction1.GetNumerator());
+        Console.WriteLine(testFraction1.GetDenominator());
+        testFraction1.SetNumerator(5);
+        testFraction1.SetDenominator(4);
         Console.WriteLine(testFraction1.GetFractionString());
         Console.WriteLine(testFraction1.GetDecimalValue());
+        Console.WriteLine();
 
+        Console.WriteLine(testFraction2.GetNumerator());
+        Console.WriteLine(testFraction2.GetDenominator());
+        testFraction2.SetNumerator(50);
+        testFraction2.SetDenominator(2);
         Console.WriteLine(testFraction2.GetFractionString());
         Console.WriteLine(testFraction2.GetDecimalValue());
+        Console.WriteLine();
 
+        Console.WriteLine(testFraction3.GetNumerator());
+        Console.WriteLine(testFraction3.GetDenominator());
+        testFraction3.SetNumerator(2);
+        testFraction3.SetDenominator(1);
         Console.WriteLine(testFraction3.GetFractionString());
         Console.WriteLine(testFraction3.GetDecimalValue());
+        Console.WriteLine();
     }
 }
