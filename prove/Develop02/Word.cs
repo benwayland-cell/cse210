ce889e950043121715687e279cb402e93b98ca11
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-12 10:15:30 -0700

    Added the files for Scripture Memorizer. Also (hopefully) finished the Word class, it's not tested yet.

---
+++ prove/Develop02/Word.cs
@@ -0,0 +1,40 @@
+
+class Word
+{
+    private string _text;
+    private bool _hidden = false;
+    private string hiddenString = "";
+
+    public Word(string text)
+    {
+        _text = text;
+
+        // make the hiddenString a string that just is a string that has
+        // as many underscores as the length of text.
+        for (int i = 0; i < _text.Count(); i++)
+        {
+            hiddenString += "_";
+        }
+    }
+
+    private void Hide()
+    {
+        _hidden = true;
+    }
+
+    private void Show()
+    {
+        _hidden = false;
+    }
+
+    public string GetWord()
+    {
+        // if it's not hidden, just return the text
+        if (!_hidden)
+        {
+            return _text;
+        }
+        // return the hidden string
+        return hiddenString;
+    }
+}
