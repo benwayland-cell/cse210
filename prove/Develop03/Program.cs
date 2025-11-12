9c563c1f48730ef9a6f32c8848caa1c793734a14
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-12 16:24:13 -0700

    Made a function that parses a string into a Reference object, also started work on a functino that parses a file into a list of scriptures

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,6 +2,115 @@ using System;
 
 class Program
 {
+    /* Takes all of the scriptures in "filename" and puts it into a list of scriptures */
+    static List<Scripture> ConvertFileToScriptures(string filename)
+    {
+        // init the list of scriptures we will return
+        List<Scripture> scriptureList = new List<Scripture>();
+
+        // Get the lines in the file
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+
+        // initialization for the loop
+
+        // is true if the next line will start a new scripture
+        bool nextLineNewScripture = true;
+        // where we'll store the reference that we will add
+        Reference newReference = null;
+        // where we'll store the verses that we will add
+        List<List<Word>> newVerses = new List<List<Word>>();
+
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                // we're at a dividing empty line so we add the scripture to the list
+                nextLineNewScripture = true;
+
+                // add the current data we have to a new scripture in scriptureList
+                if (newReference != null && newVerses.Count > 0)
+                {
+                    // !!!!!!!!!!!! Do this !!!!!!!!!!!!!
+                }
+            }
+            else if (nextLineNewScripture)
+            {
+                // we are now looking at the reference and need to parse it
+                newReference = StringToReference(line);
+            }
+            else
+            {
+                
+            }
+        }
+        return scriptureList;
+    }
+
+    /* Converts a string to a reference
+    The string must be formatted like:
+    Book chapter:startVerse-endVerse
+    or like:
+    Book chapter:startVerse
+
+    e.g.
+    1 Nephi 3:7-8
+    Mosiah 2:17
+
+    Parameters:
+        string referenceString: the string that we will be converting
+    Return:
+        A Reference object with the correct attributes
+     */
+    static Reference StringToReference(string referenceString)
+    {
+        // the book and the numbers after it
+        // the next loop will find these two
+        string book = "";
+        string referenceNumbers = "";
+
+        // search for the first number in the reference, ignore the first
+        // (because it could be something like "1 Nephi")
+        for (int charIndex = 1; charIndex < referenceString.Count(); charIndex++)
+        {
+            char currentChar = referenceString[charIndex];
+            if (Char.IsNumber(currentChar))
+            {
+                // splice the line into the book and reference numbers
+                book = referenceString.Substring(0, charIndex);
+                referenceNumbers = referenceString.Substring(charIndex);
+                break;
+            }
+        }
+
+        // splice the reference numbers so we can get the chapter and verses
+        string[] referenceNumbersSplit = referenceNumbers.Split(":");
+        int chapter = int.Parse(referenceNumbersSplit[0].Trim());
+        string verses = referenceNumbersSplit[1].Trim();
+
+        // get the start and end verse from verses
+        int startVerse;
+        int endVerse;
+
+        if (!verses.Contains("-"))
+        {
+            startVerse = int.Parse(verses);
+            endVerse = startVerse;
+        }
+        else
+        {
+            string[] versesSplit = verses.Split("-");
+            startVerse = int.Parse(versesSplit[0].Trim());
+            endVerse = int.Parse(versesSplit[1].Trim());
+        }
+
+
+
+        book = book.Trim();
+
+        return new Reference(book, chapter, startVerse, endVerse);
+    }
+
     static void Main(string[] args)
     {
         // Word testWord = new Word("test");
@@ -45,6 +154,6 @@ class Program
         // testScripture.Display();
 
 
-
+        Console.WriteLine(StringToReference("1 Nephi    3   :    7    -   8    "));
     }
 }
