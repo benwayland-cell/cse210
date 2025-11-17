798fe9a25914bea010a531fdfb958432e2326f64
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-17 09:40:39 -0700

    Initial work on WritingAssignment

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -33,3 +33,18 @@ public class MathAssignment : Assignment
         return $"Section {_textbookSection} Problems {_problems}";
     }
 }
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return _title;
+    }
+}
