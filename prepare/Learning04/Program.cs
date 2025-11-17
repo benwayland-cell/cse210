96d06702b0d3afc0b0c5238fc53299b32c73fc5d
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-17 09:44:25 -0700

    Finished writing Writing Assignment and testing for all classes, but they still have to be in one file.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,13 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Assignment testAssignment = new Assignment("test name", "test topic");
-        // Console.WriteLine(testAssignment.GetSummary());
+        Console.Clear();
+        Assignment testAssignment = new Assignment("Test assignment name", "Test assignment topic");
+        Console.WriteLine(testAssignment.GetSummary());
+        Console.WriteLine();
 
-        MathAssignment testMathAssignment = new MathAssignment("test name", "test topic", "7.3", "3-10");
+        MathAssignment testMathAssignment = new MathAssignment("Test math name", "Test math topic", "7.3", "3-10");
         Console.WriteLine(testMathAssignment.GetSummary());
         Console.WriteLine(testMathAssignment.GetHomeworkList());
+        Console.WriteLine();
 
-        
+        WritingAssignment testWritingAssignment = new WritingAssignment("Test writing name", "Test writing topic", "Test writing title");
+        Console.WriteLine(testWritingAssignment.GetSummary());
+        Console.WriteLine(testWritingAssignment.GetWritingInformation());
+        Console.WriteLine();
     }
 }
