12e69705ba5f0089e8efb3e41073b4081d638ecf
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-17 09:54:02 -0700

    Reformatted a bit and added a few comments.

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -1,3 +1,4 @@
+
 public class MathAssignment : Assignment
 {
     private string _textbookSection;
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,15 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
+
+        // test Assignment
         Assignment testAssignment = new Assignment("Test assignment name", "Test assignment topic");
         Console.WriteLine(testAssignment.GetSummary());
         Console.WriteLine();
 
+        // test MathAssignment
         MathAssignment testMathAssignment = new MathAssignment("Test math name", "Test math topic", "7.3", "3-10");
         Console.WriteLine(testMathAssignment.GetSummary());
         Console.WriteLine(testMathAssignment.GetHomeworkList());
         Console.WriteLine();
 
+        // test WritingAssignment
         WritingAssignment testWritingAssignment = new WritingAssignment("Test writing name", "Test writing topic", "Test writing title");
         Console.WriteLine(testWritingAssignment.GetSummary());
         Console.WriteLine(testWritingAssignment.GetWritingInformation());
