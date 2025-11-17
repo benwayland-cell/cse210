d87a817187b2c0a66c57af2bd6346e3c7e91378e
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-17 09:52:27 -0700

    I think I found the problem, I think I accidentally made MathAssignment file not a cs file.

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -17,34 +17,19 @@ public class Assignment
 }
 
 
-public class MathAssignment : Assignment
-{
-    private string _textbookSection;
-    private string _problems;
+// public class MathAssignment : Assignment
+// {
+//     private string _textbookSection;
+//     private string _problems;
 
-    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
-    {
-        _textbookSection = textbookSection;
-        _problems = problems;
-    }
+//     public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+//     {
+//         _textbookSection = textbookSection;
+//         _problems = problems;
+//     }
 
-    public string GetHomeworkList()
-    {
-        return $"Section {_textbookSection} Problems {_problems}";
-    }
-}
-
-public class WritingAssignment : Assignment
-{
-    private string _title;
-
-    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
-    {
-        _title = title;
-    }
-
-    public string GetWritingInformation()
-    {
-        return _title;
-    }
-}
+//     public string GetHomeworkList()
+//     {
+//         return $"Section {_textbookSection} Problems {_problems}";
+//     }
+// }
