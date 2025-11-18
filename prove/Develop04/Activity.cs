5b5c47af9ae1411f5c0cc9c85632b9adf3226817
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-18 15:16:27 -0700

    Finished and Tested StallAnimation

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,8 +1,8 @@
 
 public class Activity
 {
-    private string activityName = "empty activity name";
-    private string activityDescription = "empty activity description";
+    private string activityName = "Empty Activity Name";
+    private string activityDescription = "Empty Activity Description";
     private int timeOfActivity;
     private DateTime endLoopTime;
 
@@ -16,6 +16,7 @@ public class Activity
         
         DateTime startTime = DateTime.Now;
         endLoopTime = startTime.AddSeconds(timeOfActivity);
+        Console.WriteLine();
     }
 
     /* Counts down from lengthOfTime to 0 with text printed to the console */
@@ -27,7 +28,18 @@ public class Activity
     /* Runs a circular animation for as long as lengthOfTime */
     protected void StallAnimation(int lengthOfTime)
     {
-        
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(lengthOfTime);
+
+        string[] animationFrames = ["|", "/", "-", "\\"];
+
+        for (int animationIndex = 0; DateTime.Now < endTime; animationIndex++)
+        {
+            
+            Console.Write($"{animationFrames[animationIndex % animationFrames.Count()]}\b");
+            Thread.Sleep(500);
+        }
+        Console.WriteLine(" ");
     }
 
     /* Returns whether we have passed when the activity is to end */
@@ -36,13 +48,16 @@ public class Activity
         return DateTime.Now >= endLoopTime;
     }
 
+    /* Runs things that all activities do and runs the specific method for each activity */
     public void RunActivity()
     {
-        Console.WriteLine($"Welcome to the {activityName}\n\n{activityDescription}");
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {activityName}\n\n{activityDescription}\n");
+        GetLengthOfActivityFromUser();
         RunSpecificActivity();
 
-        Console.WriteLine("Well done!!\n");
-        Console.WriteLine($"You have completed another {timeOfActivity} seconds of the {activityName}");
+        Console.WriteLine("\nWell done!!\n");
+        Console.WriteLine($"You have completed another {timeOfActivity} seconds of the {activityName}.");
     }
 
     private void RunSpecificActivity()
@@ -50,8 +65,15 @@ public class Activity
         Console.WriteLine("Empty Activity");
 
         // test things for activity
-        GetLengthOfActivityFromUser();
-        Console.WriteLine(endLoopTime);
+
+        // GetLengthOfActivityFromUser();
+        // Console.WriteLine(endLoopTime);
+        // Thread.Sleep(2000);
+        // Console.WriteLine(CheckIfTimeIsUp());
+
+        // StallAnimation(5);
+
+        
     }
 
 }
