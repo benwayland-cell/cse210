41eaaf7eec6e6319a12aa437a25de0133fe98fcc
benwayland-cell <ben.wayland2007@gmail.com>
2025-11-19 09:34:33 -0700

    Added bones of ListingActivity.

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,21 @@
+
+public class ListingActivity : Activity
+{
+    private const string activityName = "Listing Activity";
+    private const string activityDescription = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+    
+    private string[] promptList = [
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    ];
+
+    public ListingActivity() : base(activityName, activityDescription){}
+
+    protected override void RunSpecificActivity()
+    {
+        Console.WriteLine("Run listing activity");
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Activity testActivity = new ReflectionActivity();
+        Activity testActivity = new ListingActivity();
         testActivity.RunActivity();
     }
 }
